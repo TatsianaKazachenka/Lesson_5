03a17818a4af12917071a4e9abd586c66058a050
tanuha <asus1986@tut.by>
2020-12-10 11:59:40 +0300

    Added file of constants, file for working with the driver

---
+++ src/test/java/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static final String SITE = "https://healthunify.com/bmicalculator/";
+    public static final String WEIGHT_NAME = "wg";
+    public static final String HEIGHT_NAME = "ht";
+    public static final String BTN_NAME = "cc";
+    public static final String DESC_NAME = "desc";
+    public static final String MESSAGE = "Incorrecy category is displayed:";
+
+    public static final String MESSAGE_UNDERWEIGHT = "Your category is Underweight";
+    public static final String MESSAGE_STARVATION = "Your category is Starvation";
+    public static final String MESSAGE_NORMAL = "Your category is Normal";
+    public static final String MESSAGE_OVERWEIGHT = "Your category is Overweight";
+    public static final String MESSAGE_OBESE = "Your category is Obese";
+}
